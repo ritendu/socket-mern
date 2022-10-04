d4e71aa7a59b2f81007f56cba25db59a8f1b53a4
Ritendu Sen <ritendu.sen@weavers-web.com>
2022-10-04 21:25:27 +0530

    second-commit

--- chat/backend/app.js
+++ chat/backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
+const PORT = process.env.PORT||5000;
 const app = express();
-app.listen(5000,()=>{
-    console.log('App is running');
+app.get('/',(req,res)=>{
+    res.send('App is running...')
+})
+app.listen(PORT,()=>{
+    console.log(`App is running on ${PORT}`);
 })
