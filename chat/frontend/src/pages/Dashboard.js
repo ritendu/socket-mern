913009b804d187e36155c94dde4412e67e6df594
ritendu <ritendu.sen@weavers-web.com>
2023-06-21 19:13:32 +0530

    changes

--- chat/frontend/src/pages/Dashboard.js
+++ chat/frontend/src/pages/Dashboard.js
@@ -17,15 +17,7 @@ const Dashboard = ()=>{
          {/* headaer */}
     <div class="px-5 py-5 flex justify-between items-center bg-white border-b-2">
       <div class="font-semibold text-2xl">GoingChat</div>
-      <div class="w-1/2">
-        <input
-          type="text"
-          name=""
-          id=""
-          placeholder="search IRL"
-          class="rounded-2xl bg-gray-100 py-3 px-5 w-full"
-        />
-      </div>
+ 
       {/* <div
         class="h-12 w-12 p-2 bg-yellow-500 rounded-full text-white font-semibold flex items-center justify-center"
       >
@@ -36,6 +28,7 @@ const Dashboard = ()=>{
     {/* end header */}
     {/* Chatting */}
     <div class="flex flex-row justify-between bg-white">
+     
       {/* chat list */}
       <div class="flex flex-col w-2/5 border-r-2 overflow-y-auto">
         {/* search compt  */}
@@ -48,6 +41,7 @@ const Dashboard = ()=>{
         </div>
       {/* end search compt  */}
         {/* user list */}
+        
         {data.map((data)=>{
           return (
             <div 
@@ -75,6 +69,7 @@ const Dashboard = ()=>{
       {/* end chat list */}
       {/* message */}  
       <div class="w-full px-5 flex flex-col justify-between">
+        <div>Hello</div>
         <div class="flex flex-col mt-5">
           <div class="flex justify-end mb-4">
             <div
